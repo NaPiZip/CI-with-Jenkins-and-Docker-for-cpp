d012141ec847e17037fcee91e32972b99f430227
NaPiZip <napizip@mail.com>
2019-08-04 16:33:48 +0200

    Turned building samples off.

--- build_gtest_gmock.sh
+++ build_gtest_gmock.sh
@@ -2,9 +2,8 @@
 
 cd googletest
 # In order to display all cmake vars use 'cmake -LAH'
-
 .././dockcross cmake -Bbuild -H. '-GUnix Makefiles' \
-      -Dgtest_build_samples=ON \
+      -Dgtest_build_samples=OFF \
       -Dgtest_build_tests=ON \
       -Dgmock_build_tests=ON \
       -Dcxx_no_exception=OFF \
@@ -12,5 +11,5 @@ cd googletest
       -DCMAKE_COMPILER_IS_GNUCXX=OFF
 
 .././dockcross make -Cbuild -i
-.././dockcross make test -Cbuild 
+.././dockcross make test -Cbuild
 cd ..
