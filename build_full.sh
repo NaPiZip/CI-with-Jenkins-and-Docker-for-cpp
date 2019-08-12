4e0ef0d08b11555332930b5ad43259c04274348c
NaPiZip <napizip@mail.com>
2019-08-12 22:25:04 +0200

    Added first bugfix for building in Linux and added platform dependednd modifications.

--- build_full.sh
+++ build_full.sh
@@ -10,17 +10,30 @@ if [ ! -f ./dockcross ]; then
 
   docker run --rm dockcross/$CROSS_COMPILER_IMAGE_NAME > ./dockcross_pre
 
-  # Fixing a platform error see Readme for details
-  awk '!/HOST_PWD=\$\{HOST_PWD\/\\/ {print $0}' dockcross_pre > ./dockcross_sh
-  #rm dockcross_pre
+  # Bash on Ubuntu
+  LINUX=$(uname -o || awk '/Msys/ {print $0}')
+  # MSYS, Git Bash, etc.
+  MSYS=$(uname -o || awk '/Msys/ {print $0}')
+
+  if [ -z "$DOCKER_TOOLBOX_INSTALL_PATH" -a -n "$LINUX" ]; then
+    echo "Target is LINUX"
+    # Fixing platform error for Docker inside the VM
+    awk '!/USER_IDS=\(-e BUILDER_UID/ {print $0}' dockcross_pre > ./dockcross_sh
+  elif [ -n "$MSYS" ]; then
+    echo "Target is MSYS"
+    # Fixing a platform error on MSYS see Readme for details
+    awk '!/HOST_PWD=\$\{HOST_PWD\/\\/ {print $0}' dockcross_pre > ./dockcross_sh
+  fi
+
+  rm dockcross_pre
   chmod +x ./dockcross_sh
   echo "Done creating dockcross script!"
 else
   echo "Skipped creating dockcross script, since it already exists!"
 fi
 
-./dockcross_sh cmake -H. -Bbuild "-GUnix Makefiles"
-./dockcross_sh make -Cbuild
-./dockcross_sh make test -Cbuild
+#./dockcross_sh cmake -H. -Bbuild "-GUnix Makefiles"
+#./dockcross_sh make -Cbuild
+#./dockcross_sh make test -Cbuild
 
 echo "Successfully terminated build.sh script!!"
