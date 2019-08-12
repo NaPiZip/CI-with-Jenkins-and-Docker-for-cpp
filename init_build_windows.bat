6423f97f306bb89e16af9403fcf67060a01088b7
NaPiZip <napizip@mail.com>
2019-08-12 22:25:42 +0200

    Fixed correct file reference to build_full.sh .

--- init_build_windows.bat
+++ init_build_windows.bat
@@ -3,4 +3,4 @@ set "CWD=%CWD:\=/%"
 
 cd /d "C:\Program Files\Docker Toolbox"
 
-"C:\Program Files\Git\bin\bash.exe" --login -i "C:\Program Files\Docker Toolbox\start.sh" %CWD%/build.sh
+"C:\Program Files\Git\bin\bash.exe" --login -i "C:\Program Files\Docker Toolbox\start.sh" %CWD%/build_full.sh
