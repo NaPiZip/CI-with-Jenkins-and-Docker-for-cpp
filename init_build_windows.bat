971065a3000f758293c2ed1f89a2fafbc2bb64bf
NaPiZip <napizip@mail.com>
2019-08-18 21:09:31 +0200

    Removed standalone build in Windows.

--- init_build_windows.bat
+++
@@ -1,6 +0,0 @@
-set "CWD=%cd%"
-set "CWD=%CWD:\=/%"
-
-cd /d "C:\Program Files\Docker Toolbox"
-
-"C:\Program Files\Git\bin\bash.exe" --login -i "C:\Program Files\Docker Toolbox\start.sh" %CWD%/build_full.sh
